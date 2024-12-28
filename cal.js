5c57d710f7cf12a0b33e8fc594f51ce7c3d7d574
nikhil0441 <nikhilpatel03022004@gmail.com>
2024-12-28 14:46:57 +0530

    first commit

---
+++ cal.js
@@ -0,0 +1,25 @@
+let ans = "";
+let pressed = (value) => {
+  let show = document.querySelector("#display");
+  ans = ans + value;
+  show.innerHTML = ans;
+};
+
+let equall = () => {
+  let show = document.querySelector("#display");
+  show.innerHTML = eval(ans);
+  ans = eval(ans);
+  ans = ans.toString();
+};
+
+let del = () => {
+  let show = document.querySelector("#display");
+  ans = ans.slice(0, -1);
+  show.innerHTML = ans;
+};
+
+let clearr = () => {
+  let show = document.querySelector("#display");
+  ans = "";
+  show.innerHTML = "";
+};
